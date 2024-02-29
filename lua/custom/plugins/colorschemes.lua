08fc7756190301c222e2617e79a8a7c9ddcd1916
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-28 18:57:54 -0800

    feat: add nordic colorscheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,12 +1,14 @@
 return {
   {
-    'olimorris/onedarkpro.nvim',
+    'AlexvZyl/nordic.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'nordic'
     end,
   },
+
+  'olimorris/onedarkpro.nvim',
 }
 -- vim: ts=2 sts=2 sw=2 et
