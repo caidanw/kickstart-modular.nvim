7be3c63eb4bcd3fb43e7de58a4c6b58ed1e31fce
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-01 14:57:09 -0800

    feat: enable treesitter incremental selection

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -12,6 +12,15 @@ return {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = false,
+            node_decremental = '<backspace>',
+          },
+        },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact
