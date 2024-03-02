1b73b1961a3654ab4fd938d01dc94645316d7089
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-01 16:25:04 -0800

    refactor: undotree to own plugin file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,8 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  { 'mbbill/undotree', keys = {
-    { '<leader>u', vim.cmd.UndotreeToggle, desc = 'Toggle [U]ndotree' },
-  } },
-}
+return {}
