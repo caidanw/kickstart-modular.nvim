b8289423c21b2203c56bd4f64010505096188e9f
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-01 16:25:21 -0800

    fix: nordic colorscheme options

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -4,8 +4,13 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here
-      vim.cmd.colorscheme 'nordic'
+      -- Load the colorscheme with custom options
+      require('nordic').load {
+        bright_border = true,
+        telescope = {
+          style = 'classic',
+        },
+      }
     end,
   },
 
