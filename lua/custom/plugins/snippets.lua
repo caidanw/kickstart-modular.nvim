3d12e5af04af7aec61efa6c1c36c43b8ac223f17
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-04-29 13:29:51 -0700

    fix: snippets

--- lua/custom/plugins/snippets.lua
+++ lua/custom/plugins/snippets.lua
@@ -1,4 +1,9 @@
 return {
-  'L3MON4D3/LuaSnip',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    init = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
 }
