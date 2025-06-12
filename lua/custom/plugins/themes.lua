44acedf77ab915472b25baccece1b8cd2bfe53ce
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-06-12 13:19:11 -0700

    feat: add AlexvZyl/nordic.nvim colorscheme

---
+++ lua/custom/plugins/themes.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      bold_keywords = false,
+      bright_border = false,
+      cursorline = {
+        theme = 'light',
+        -- blend = 1.5,
+      },
+      telescope = {
+        style = 'classic',
+      },
+      after_palette = function(palette)
+        -- Fix the border colors from being too dark
+        palette.border_fg = palette.grey5
+        palette.fg_float_border = palette.grey5
+        palette.fg_popup_border = palette.grey5
+      end,
+    },
+    config = function(opts)
+      require('nordic').load(opts)
+    end,
+  },
+}
