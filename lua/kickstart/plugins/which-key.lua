818bce8ec42a66c12e35cd29aa5bf3278e69adc3
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-06-17 16:10:16 -0700

    feat: create which-key group for lazygit

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -19,7 +19,7 @@ return {
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
+      delay = 200,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
@@ -63,6 +63,7 @@ return {
         { '<leader>t', group = '[T]oggle' },
         { '<leader>c', group = '[C]ode', mode = { 'n', 'v' } },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>g', group = '[G]it Tools', mode = { 'n', 'v' } },
       },
     },
   },
