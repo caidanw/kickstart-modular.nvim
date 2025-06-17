6726bdb2703820b6e6da7e7b372a2bf87d8acf81
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-06-17 13:26:30 -0700

    feat: add autocmd for building spl file

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -12,4 +12,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Ensure that the binary spl file is up-to-date with the source add file
+-- Source: https://www.lorenzobettini.it/2025/01/automatically-regenerating-neovim-spell-files/
+vim.api.nvim_create_autocmd('FocusGained', {
+  pattern = '*',
+  callback = function()
+    local config_path = vim.fn.stdpath 'config' -- Get Neovim's config path
+    local add_file = config_path .. '/spell/en.utf-8.add'
+    local spl_file = config_path .. '/spell/en.utf-8.add.spl'
+
+    if vim.fn.filereadable(add_file) == 1 then
+      local add_mtime = vim.fn.getftime(add_file) -- Get modification time of .add file
+      local spl_mtime = vim.fn.getftime(spl_file) -- Get modification time of .add.spl file
+
+      -- Run mkspell! only if .add is newer than .add.spl or .add.spl doesn't exist
+      if add_mtime > spl_mtime or spl_mtime == -1 then
+        vim.cmd('silent! mkspell! ' .. spl_file .. ' ' .. add_file)
+      end
+    end
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
