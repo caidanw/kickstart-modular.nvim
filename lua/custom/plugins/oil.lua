857be21961a57ee7b612e90d2b51b07779b2210f
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-06-17 13:25:08 -0700

    feat: change settings for oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,11 +1,38 @@
+---@module 'lazy'
+---@type LazySpec
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    -- Oil will take over directory buffers (e.g. `nvim .` or `:e src/`)
+    -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+    default_file_explorer = true,
+    -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+    delete_to_trash = true,
+    -- Set to true to watch the filesystem for changes and reload oil
+    watch_for_changes = true,
+
+    keymaps = {
+      ['q'] = { 'actions.close', mode = 'n' },
+      ['<M-h>'] = { 'actions.toggle_hidden', mode = 'n' },
+      ['Y'] = { 'actions.yank_entry', mode = 'n' },
+      ['gd'] = {
+        function()
+          _G.oil_columns = not _G.oil_columns
+          local columns = _G.oil_columns and { 'icon', 'permissions', 'size', 'mtime' } or { 'icon' }
+          require('oil').set_columns(columns)
+        end,
+        desc = 'Toggle columns',
+      },
+    },
+  },
   -- Optional dependencies
   -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  keys = {
+    { '<leader>e', '<CMD>Oil<CR>', mode = 'n', desc = 'Open Oil' },
+  },
 }
