074b98b6f444e44d0bbe935956d687710a2aa50d
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-06-17 13:26:48 -0700

    feat: add autocmd for disabling LSP diagnostics on env files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -33,4 +33,13 @@ vim.api.nvim_create_autocmd('FocusGained', {
   end,
 })
 
+-- Create an autocommand to disable LSP diagnostics for .env and *.env.* files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  group = vim.api.nvim_create_augroup('DisableLspDiagnosticsForEnv', { clear = true }),
+  pattern = { '.env', '.env.*' },
+  callback = function(args)
+    vim.diagnostic.enable(false, { bufnr = args.buf })
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
