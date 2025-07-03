09cb507c755a70af5edb86a59a8aa60d6f43df97
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-07-03 11:30:55 -0700

    feat: update lspconfig for Lua to use Neovim runtime files

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -229,6 +229,14 @@ return {
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+              },
+              -- Enable hover documentation
+              hover = { enable = true },
+              signature = { enable = true },
             },
           },
         },
