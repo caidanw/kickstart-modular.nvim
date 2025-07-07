75102edc241356679a1cffb40d75a7d9f6811f71
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-07-07 10:47:30 -0700

    refactor: snacks.lua pickers descriptions

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -144,23 +144,21 @@ return {
     { '<leader>gf', function() Snacks.lazygit.log_file() end, desc = 'LazyGit File View' },
 
     --[[ Pickers ]]
+    { '<leader><leader>', function() Snacks.picker.buffers() end, desc = 'Find existing buffers' },
+    { '<leader>/', function() Snacks.picker.lines({}) end, desc = 'Fuzzily search in current buffer' },
     { '<leader>e', function() Snacks.picker.explorer() end, desc = 'Snacks Explorer' },
-    { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp' },
-    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps' },
-    { '<leader>sf', function() Snacks.picker.smart() end, desc = '[S]earch [F]iles' },
-    { '<leader>ss', function() Snacks.picker.pickers() end, desc = '[S]earch [S]elect Snacks' },
-    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch current [W]ord', mode = { 'n', 'x' } },
-    { '<leader>sg', function() Snacks.picker.grep() end, desc = '[S]earch by [G]rep' },
-    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics' },
-    { '<leader>sr', function() Snacks.picker.resume() end, desc = '[S]earch [R]esume' },
-    { '<leader>s.', function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
-    { '<leader><leader>', function() Snacks.picker.buffers() end, desc = '[ ] Find existing buffers' },
-    { '<leader>/', function() Snacks.picker.lines({}) end, desc = '[/] Fuzzily search in current buffer' },
-    { '<leader>s/', function() Snacks.picker.grep_buffers() end, desc = '[S]earch [/] in Open Files' },
-    -- Shortcut for searching your Neovim configuration files
-    { '<leader>sn', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = '[S]earch [N]eovim files' },
-    { '<leader>uC', function() Snacks.picker.colorschemes() end },
+    { '<leader>sh', function() Snacks.picker.help() end, desc = 'Help' },
+    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = 'Keymaps' },
+    { '<leader>sf', function() Snacks.picker.smart() end, desc = 'Files' },
+    { '<leader>ss', function() Snacks.picker.pickers() end, desc = 'Snacks Pickers' },
+    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Current Word', mode = { 'n', 'x' } },
+    { '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
+    { '<leader>sr', function() Snacks.picker.resume() end, desc = 'Resume' },
+    { '<leader>s.', function() Snacks.picker.recent() end, desc = 'Recent Files ("." for repeat)' },
+    { '<leader>sn', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = 'Neovim Files' },
     { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
+    { '<leader>uC', function() Snacks.picker.colorschemes() end, desc = 'Colorscheme with Preview' },
 
     --[[ Misc ]]
     {
