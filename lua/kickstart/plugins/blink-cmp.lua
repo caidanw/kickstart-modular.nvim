1bb4d93dc28cbe32b5bd922e1dffa3936b3d6421
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-07-09 11:33:15 -0700

    feat: enable auto show documentation in blink-cmp

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -12,7 +12,9 @@ return {
           -- Build Step is needed for regex support in snippets.
           -- This step is not supported in many windows environments.
           -- Remove the below condition to re-enable on windows.
-          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then return end
+          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
+            return
+          end
           return 'make install_jsregexp'
         end)(),
         dependencies = {
@@ -70,7 +72,10 @@ return {
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
       },
 
       sources = {
