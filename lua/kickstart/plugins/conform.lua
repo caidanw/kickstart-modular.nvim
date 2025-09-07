7960b1c15d1d22bbc2ab77c766d7be56c5727a31
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-09-07 13:44:32 -0700

    feat(plugins): improve conform autoformat control and update python formatter
    - Add `notify_no_formatters` option to conform setup
    - Refactor autoformat disabling logic to use filetype list and global/buffer variables
    - Change python formatter from `ruff` to `ruff_format`

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -18,26 +18,29 @@ return {
     ---@module 'conform'
     ---@type conform.setupOpts
     opts = {
+      notify_no_formatters = true,
       notify_on_error = true,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { 'c', 'cpp' }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
         end
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
 
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff' },
+        python = { 'ruff_format' },
 
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
