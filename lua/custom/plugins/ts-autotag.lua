652a67cbc8e4d69fc57a782903bb73c2206cdf15
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-09-24 14:02:46 -0700

    feat: add plugin nvim-ts-autotag

---
+++ lua/custom/plugins/ts-autotag.lua
@@ -0,0 +1,14 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'windwp/nvim-ts-autotag',
+  lazy = false,
+  opts = {
+    opts = {
+      -- Defaults
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false, -- Auto close on trailing </
+    },
+  },
+}
