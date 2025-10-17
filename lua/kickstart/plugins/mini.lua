8c224bef695926c1390314ad20a94e6c9d3a01d3
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-10-17 13:05:25 -0700

    fix: disable mini.hipatterns

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -143,40 +143,40 @@ return {
         return '%2l:%-2v'
       end
 
-      -- Highlight Patterns for hex colors and comments
-      local hipatterns = require('mini.hipatterns')
-      hipatterns.setup({
-        highlighters = {
-          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-          -- fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-          -- hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-          -- todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-          -- note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
-
-          -- Highlight hex color strings (`#rrggbb`) using that color
-          hex_color = hipatterns.gen_highlighter.hex_color(),
-        },
-        tailwind = {
-          enabled = true,
-          ft = {
-            'astro',
-            'css',
-            'heex',
-            'html',
-            'html-eex',
-            'javascript',
-            'javascriptreact',
-            'rust',
-            'svelte',
-            'typescript',
-            'typescriptreact',
-            'vue',
-          },
-          -- full: the whole css class will be highlighted
-          -- compact: only the color will be highlighted
-          style = 'full',
-        },
-      })
+      -- -- Highlight Patterns for hex colors and comments
+      -- local hipatterns = require('mini.hipatterns')
+      -- hipatterns.setup({
+      --   highlighters = {
+      --     -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      --     -- fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+      --     -- hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+      --     -- todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+      --     -- note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+      --
+      --     -- Highlight hex color strings (`#rrggbb`) using that color
+      --     hex_color = hipatterns.gen_highlighter.hex_color(),
+      --   },
+      --   tailwind = {
+      --     enabled = true,
+      --     ft = {
+      --       'astro',
+      --       'css',
+      --       'heex',
+      --       'html',
+      --       'html-eex',
+      --       'javascript',
+      --       'javascriptreact',
+      --       'rust',
+      --       'svelte',
+      --       'typescript',
+      --       'typescriptreact',
+      --       'vue',
+      --     },
+      --     -- full: the whole css class will be highlighted
+      --     -- compact: only the color will be highlighted
+      --     style = 'full',
+      --   },
+      -- })
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
